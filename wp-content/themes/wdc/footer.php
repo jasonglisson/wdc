4f460bf04993fa426574ab919d739f1f3f618cbe
jasonglisson <jason.glisson@gmail.com>
2015-04-16 08:28:06 -0400

    Added favicon

--- wp-content/themes/wdc/footer.php
+++ wp-content/themes/wdc/footer.php
@@ -9,9 +9,15 @@
 		    				</div>
 			               
 			                <div class="large-12 medium-12 columns">
-				                <div class="small-8 small-centered columns">		
-									<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+				                <div class="large-5 small-12 columns copyright">		
+									&copy; 2015 Wilmington design co. All rights reserved.
 				                </div>
+				                <div class="large-2 small-12 columns footer-icon">
+					                <img src="<?php echo get_template_directory_uri(); ?>/library/images/footer-icon.png">
+				                </div>
+				                <div class="large-5 small-12 columns services">		
+				                	NC Web | Branding | Creative | Marketing 
+				                </div>				                
 							</div>		
 						</div> <!-- end #inner-footer -->			
 					</footer> <!-- end .footer -->
